c6324103595322384346f8922cca81c183a359be
QianYu <1090163768@qq.com>
2018-04-25 11:10:44 +0800

    Add files via upload

---
+++ SecyuXibCategory/SecyuXibCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SecyuXibCategory
+//
+//  Created by TW-YQ on 2018/4/25.
+//  Copyright © 2018年 secyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SecyuXibCategory/SecyuXibCategory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SecyuXibCategory
+//
+//  Created by TW-YQ on 2018/4/25.
+//  Copyright © 2018年 secyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
